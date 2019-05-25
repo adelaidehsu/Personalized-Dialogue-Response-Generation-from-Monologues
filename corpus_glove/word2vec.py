a0c1db01811f7ae75d852445b25984366196c13c
hsuadelaide <hsu.adelaide@gmail.com>
2019-05-25 15:40:45 +0800

    update

--- corpus_glove/word2vec.py
+++
@@ -1,7 +0,0 @@
-from gensim.models import word2vec
-import logging
- 
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-sentences = word2vec.Text8Corpus('text8')
-print(sentences)
-model = word2vec.Word2Vec(sentences, size=200)
